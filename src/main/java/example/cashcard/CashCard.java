vmware-tanzu-learning/course-spring-brasb-build-a-rest-api-code

<test-first-end, first-rest-endpoint-start> Completed JSON tests for CashCard data contract

src/main/java/example/cashcard/CashCard.java
@@ -1,0 +1,4 @@
+package example.cashcard;
+
+public record CashCard(String id, Double amount) {
+}

mianmianguo2020/SprintbootV3

src/main/java/example/cashcard/CashCard.java
@@ -1,0 +1,4 @@
+package example.cashcard;
+
+public record CashCard(Long id, Double amount) {
+}